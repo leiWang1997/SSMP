3a20d18f6cd0c821cd923ca2ff2df837d1bbb82f
wanglei <wanglei@113>
2023-05-13 01:37:47 +0800

    2

--- SimpleWeb/src/main/java/com/wanglei/SimpleWebApplication.java
+++ SimpleWeb/src/main/java/com/wanglei/SimpleWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleWebApplication {
 
     public static void main(String[] args) {
+int i = 0;
         SpringApplication.run(SimpleWebApplication.class, args);
     }
 
